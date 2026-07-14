8281d5471c4f482544b8642b0b5827a4b1c45abe
Serov Konstantin <KageMage007@yandex.ru>
2026-07-14 08:27:09 +0300

    Добавить новый файл - .gitignore. Изменить код в файле main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello from daun!')
+# Импорт load_dotenv.
+from dotenv import load_dotenv
+
+import os
+
+def print_author():
+    # Загрузка переменных из .env
+    load_dotenv(dotenv_path='D:/Учёба/Практикум-Data_Scientist/Спринт_8/my_git_project/new_file.env')
+    author = os.getenv('AUTHOR')
+    print(f"Автор проекта: {author}")
+
+print_author()
